a6e874bf4391c1615185c5d47d0e8d394650cc00
Matthew Campbell <m.campbell92@gmail.com>
2024-11-18 12:31:57 +0100

    add button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,8 @@
-export function Button() {
-  return <button></button>;
-}
+import { ComponentProps } from "react";
+import "./styles.module.css";
+
+type ButtonProps = ComponentProps<"button">;
+
+export const Button = ({ ...props }: ButtonProps) => {
+  return <button {...props} />;
+};
